49e809e98a3a81014115f098643d0f45eed23d02
Agnish Ghosh <iamagnix@gmail.com>
2023-10-27 12:35:04 +0530

    add: types for verkle and ipa

---
+++ eth_verkle/verkle_types/types.nim
@@ -0,0 +1,48 @@
+import
+  ../../constantine/constantine/platforms/[primitives],
+  ../../constantine/constantine/math/config/[type_ff, curves],
+  ../../constantine/constantine/math/elliptic/[ec_twistededwards_projective],
+  ../../constantine/constantine/math/arithmetic,
+  ../../constantine/constantine/math/io/[io_fields],
+  ../../constantine/constantine/curves_primitives
+
+type
+  EC_P* = ECP_TwEdwards_Prj[Fp[Banderwagon]]
+  EC_P_Fr* = Fr[Banderwagon]
+
+type 
+    IPAProof* = object
+     L_vector*: array[8,EC_P]
+     R_vector*: array[8,EC_P]
+     A_scalar*: EC_P_Fr
+
+type 
+    MultiProof* = object
+     IPAprv*: IPAProof
+     D*: EC_P
+
+const
+ DOMAIN*: int = 256
+
+type 
+ PrecomputedWeights* = object
+  barycentricWeights*: array[512,EC_P_Fr]
+  invertedDomain*: array[510,EC_P_Fr]
+
+type
+   IPASettings* = object
+    SRS*: array[DOMAIN,EC_P]
+    Q_val*: EC_P
+    precompWeights*: PrecomputedWeights
+    numRounds*: uint64
+
+const seed* = asBytes"eth_verkle_oct_2021"
+
+type Bytes* = array[32, byte]
+
+type
+    Coord* = object 
+     x*: EC_P_Fr
+     y*: EC_P_Fr
+
+var generator* = Banderwagon.getGenerator()
